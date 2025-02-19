dbdafaf7a343b4b04f3f14ff92f09b3db81a0cad
NejraStrsevic <strsevicnejra@gmail.com>
2025-02-19 15:10:51 +0100

    Demo event after professor registration to the course

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,8 +2,14 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
+@EnableAspectJAutoProxy
 public class DemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/demo/services/ProfesorService.java
+++ src/main/java/com/example/demo/services/ProfesorService.java
@@ -3,6 +3,7 @@ package com.example.demo.services;
 import com.example.demo.entities.Profesor;
 import com.example.demo.entities.courses.Course;
 import com.example.demo.entities.student.CourseRegistration;
+import com.example.demo.event.ProfessorRegistrationEvent;
 import com.example.demo.exception.BadRequestException;
 import com.example.demo.model.CourseDto;
 import com.example.demo.model.CourseMapper;
@@ -10,9 +11,13 @@ import com.example.demo.model.ProfesorDto;
 import com.example.demo.model.ProfessorMapper;
 import com.example.demo.repositories.CourseRegistrationRepository;
 import com.example.demo.repositories.ProfesorRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.event.TransactionPhase;
+import org.springframework.transaction.event.TransactionalEventListener;
 
 import java.util.List;
 
@@ -24,13 +29,15 @@ public class ProfesorService {
     private final CourseRegistrationRepository courseRegistrationRepository;
     private final CourseService courseService;
     private final CourseMapper courseMapper;
+    private final ApplicationEventPublisher eventPublisher;
 
-    public ProfesorService(ProfesorRepository profesorRepository, ProfessorMapper professorMapper, CourseRegistrationRepository courseRegistrationRepository, CourseService courseService, CourseMapper courseMapper) {
+    public ProfesorService(ProfesorRepository profesorRepository, ProfessorMapper professorMapper, CourseRegistrationRepository courseRegistrationRepository, CourseService courseService, CourseMapper courseMapper, ApplicationEventPublisher eventPublisher) {
         this.profesorRepository = profesorRepository;
         this.professorMapper = professorMapper;
         this.courseRegistrationRepository = courseRegistrationRepository;
         this.courseService = courseService;
         this.courseMapper = courseMapper;
+        this.eventPublisher = eventPublisher;
     }
 
     public ProfesorDto addNewProfessor(ProfesorDto profesorDto) {
@@ -83,6 +90,22 @@ public class ProfesorService {
 
     }
 
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+
+    public void writeProfessorName(ProfessorRegistrationEvent professorRegistrationEvent) {
+        System.out.println(professorRegistrationEvent.name());
+
+    }
+
+//    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+//
+//    public void writeProfessorNameTwice(ProfessorRegistrationEvent professorRegistrationEvent) {
+//        System.out.println(professorRegistrationEvent.name());
+//        System.out.println(professorRegistrationEvent.name());
+//
+//    }
+
+    @Transactional
     public void assignProfesorToCourse(Integer courseId, Integer profesorId) {
 
         List<CourseRegistration> profesorCourse = courseRegistrationRepository.findByCourseId(Long.valueOf(courseId));
@@ -94,6 +117,9 @@ public class ProfesorService {
             courseRegistration.setProfesor(profesor);
         }
         courseRegistrationRepository.saveAll(profesorCourse);
+
+        assert profesor != null;
+        eventPublisher.publishEvent(new ProfessorRegistrationEvent(profesor.getName()));
     }
 
     public Page<CourseDto> getAllProfesorCourses(Integer profesorId, Pageable pageable) {
